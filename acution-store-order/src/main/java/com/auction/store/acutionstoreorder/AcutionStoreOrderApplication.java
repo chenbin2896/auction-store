3c9cd6e148140110da7516acfef726d591722ad8
EDZ <3375793023@qq.com>
2019-09-27 18:23:52 +0800

    init 原型

---
+++ acution-store-order/src/main/java/com/auction/store/acutionstoreorder/AcutionStoreOrderApplication.java
@@ -0,0 +1,13 @@
+package com.auction.store.acutionstoreorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcutionStoreOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcutionStoreOrderApplication.class, args);
+    }
+
+}

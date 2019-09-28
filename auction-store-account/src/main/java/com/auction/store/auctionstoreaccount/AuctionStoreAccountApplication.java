cfcbebc9353f522600b2c7f5b48e6d8b251650d6
chenbin <3375793023@qq.com>
2019-09-28 11:53:51 +0800

    init config

--- auction-store-account/src/main/java/com/auction/store/auctionstoreaccount/AuctionStoreAccountApplication.java
+++ auction-store-account/src/main/java/com/auction/store/auctionstoreaccount/AuctionStoreAccountApplication.java
@@ -1,8 +1,11 @@
 package com.auction.store.auctionstoreaccount;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +18,12 @@ public class AuctionStoreAccountApplication {
         SpringApplication.run(AuctionStoreAccountApplication.class, args);
     }
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @RequestMapping("/test")
     public String demo () {
+        jdbcTemplate.query("select * from t_user ",new BeanPropertyRowMapper<>(String.class));
         return "gfadkgkjhlksf";
     }
 }

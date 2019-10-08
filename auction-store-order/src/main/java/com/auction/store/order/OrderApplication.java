48bd3e49691bb7a3b05f2e75f925acdac4dd58a0
chenbin <3375793023@qq.com>
2019-10-08 08:22:42 +0800

    update

--- auction-store-order/src/main/java/com/auction/store/order/OrderApplication.java
+++ auction-store-order/src/main/java/com/auction/store/order/OrderApplication.java
@@ -3,6 +3,7 @@ package com.auction.store.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author User
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class OrderApplication {
 
     public static void main(String[] args) {
